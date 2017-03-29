ecc1717943232d74b579fd21597abc405197dfc1
RoyalSix <jay.a.scottjr@biola.edu>
2017-03-29 16:28:19 -0700

    added comments to chapelRedcer

--- src/js/Reducers/chapelReducer.js
+++ src/js/Reducers/chapelReducer.js
@@ -1,15 +1,28 @@
 import * as types from '../Actions/actionTypes';
+//This will give us constant strings so that we are not 
+//Just creating types out of thin air that may or may not be used somewhere else
 
+//This is the initial state that will be in the store for the chapelReducer
+//You should have every field here that you might expect to present state
+//sometime during the lifetime of the app
 export function chapelReducer(state = {
     chapels: [],
     loadingChapels: false
 }, action) {
     switch (action.type) {
         case types.START_CHAPEL_LOAD:
+        //This types.XXXX should be the same as
+        //the corresponding action you are dispatching
             return {
                 ...state,
                 loadingChapels: true
             }
+            /*...state is javascript ES6 syntax for the spread operator
+             * Just think of it as copying over everything from the state
+             * and merging it will the new fields specified
+             * {@see http://redux.js.org/docs/recipes/UsingObjectSpreadOperator.html}
+             * for more information
+             */
             break;
         case types.RECIEVE_CHAPEL_LOAD:
             return {
