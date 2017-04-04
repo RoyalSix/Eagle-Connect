f7c6bad971f2ce73e1695cfa4a946183f3bafeb3
SeanMcCarrell <seanmccarrell@yahoo.com>
2017-04-04 09:58:06 -0700

    Worked on parsing HTML. Check chapelActions.js

--- src/js/Components/Events.js
+++ src/js/Components/Events.js
@@ -1,4 +1,3 @@
-//Importing what I need from React, React-native, and CSS
 import React, { Component } from 'react';
 import {
     Text,
@@ -7,9 +6,33 @@ import {
 } from 'react-native';
 import style from 'css';
 
-//Create component to be rendered in container and send down props for data
 export default class EventContainer extends Component {
-    
+    renderRow(data) {
+        return (
+            <View style={{ paddingVertical: 10, paddingHorizontal: 5, borderRadius: 5 }}>
+                <Text style={{ fontSize: 20 }}>{data.date}</Text>
+                <Text style={{ fontSize: 15 }}>{data.time}</Text>
+                <Text style={{ fontSize: 15 }}>{data.title}</Text>
+                <Text style={{ fontSize: 12 }}>{data.speaker}</Text>
+            </View>
+        )
+    }
+    renderHeader() {
+        return <Text style={style.eventHeading}>EVENTS</Text>;
+    }
+    renderSeparator(sectionId, rowId){
+        return <View key={rowId} style={style.eventSeparator} />;
+    }
+    render() {
+        return (
+            <ListView
+                enableEmptySections
+                style={style.eventContainer}
+                dataSource={this.props.dataSource}
+                renderRow={this.renderRow}
+                renderSeparator={this.renderSeparator}
+                renderHeader={this.renderHeader}
+            />
+        )
+    }
 }
-
-
