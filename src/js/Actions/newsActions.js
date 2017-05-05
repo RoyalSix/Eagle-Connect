9bcee48619573dd876fc7476cae66df3cd3f59fe
RoyalSix <jay.a.scottjr@biola.edu>
2017-05-04 19:38:50 -0700

    added fix for parsehub api not working

--- src/js/Actions/newsActions.js
+++ src/js/Actions/newsActions.js
@@ -100,6 +100,7 @@ export function getNewsItems(callback) {
 
 export function getExtraNewsItems(callback) {
     API.getHTMLFromURL('https://www.parsehub.com/api/v2/projects/t6wjq5ENy15n/last_ready_run/data?&format=json&api_key=tYB1vcfaP10q', function (htmlString) {
+        try {
         const newsItems = JSON.parse(htmlString).newsitem;
         var newsobjects = [];
         for (var item of newsItems) {
@@ -126,5 +127,8 @@ export function getExtraNewsItems(callback) {
             })
         }
         callback(newsobjects);
+        } catch(e) {
+            
+        }
     });
 }
