adc292d5e9f446f24ba88aa2ce0296762dfbd8e3
RoyalSix <jay.a.scottjr@biola.edu>
2017-05-23 13:18:55 -0700

    enabled facebook for android

--- android/app/src/main/java/com/eagleconnect/MainApplication.java
+++ android/app/src/main/java/com/eagleconnect/MainApplication.java
@@ -11,10 +11,21 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.FacebookSdk;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -26,7 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new FBSDKPackage()
+            new FBSDKPackage(mCallbackManager)
       );
     }
   };
